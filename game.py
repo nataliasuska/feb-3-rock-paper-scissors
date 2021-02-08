8b2fb8906002d9e0f011edc01cb41b8e5b237186
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-02-08 18:20:59 -0500

    Added in Nat Bot's Choice

--- game.py
+++ game.py
@@ -1,16 +1,19 @@
+import random
 
 print("-------------------")
 print("Welcome 'Player One' to a Rock-Paper-Scissors game with Nat!")
 print("-------------------")
 
 human_choice = input("Please select either rock, paper, or scissors")
-print("You chose:", human_choice)
+print(f"You chose: {human_choice}")
 
-exit()
+choices = ['rock', 'paper', 'scissors']
+natbot_choice = random.choice(choices)
+print(f"Nat chose: {natbot_choice}")
 
-print("You chose: rock")
-print("The computer chose: paper")
 print("-------------------")
+
+
 print("Oh, the computer won. It's ok.")
 print("-------------------")
 print("Thanks for playing. Please play again!")
