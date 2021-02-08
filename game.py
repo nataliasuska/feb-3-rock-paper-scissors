62560663101f7cc262ab7992e817b7fd142c60db
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-02-08 18:32:06 -0500

    Added in Winning Criteria
    We can now determine a winner.

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ print("-------------------")
 print("Welcome 'Player One' to a Rock-Paper-Scissors game with Nat!")
 print("-------------------")
 
-human_choice = input("Please select either rock, paper, or scissors")
+human_choice = input("Please select either rock, paper, or scissors:")
 print(f"You chose: {human_choice}")
 
 choices = ['rock', 'paper', 'scissors']
@@ -13,7 +13,17 @@ print(f"Nat chose: {natbot_choice}")
 
 print("-------------------")
 
+if natbot_choice == "scissors" and human_choice == "rock":
+    print("Congrats! You Won!")
+elif natbot_choice == "rock" and human_choice == "paper":
+    print("Congrats! You Won!")
+elif natbot_choice == "paper" and human_choice == "scissors":
+    print("Congrats! You Won!")
+elif natbot_choice == human_choice:
+    print("It's a tie!")
+else:
+    print("Oh, Nat won. Please don't cry...")
 
-print("Oh, the computer won. It's ok.")
 print("-------------------")
-print("Thanks for playing. Please play again!")
+print("Thanks for playing, I hope you had fun here. Please play again soon!")
+print("-------------------")
