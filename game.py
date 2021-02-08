497167cc8807c2a3380b01901e16597998005f69
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-02-08 18:18:16 -0500

    Added in User Choice
    Trying to remember exactly how I changed everything in order

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
 
--------------------
-Welcome 'Player One' to my Rock-Paper-Scissors game...
--------------------
-Please choose either 'rock', 'paper', or 'scissors': rock
-You chose: 'rock'
-The computer chose: 'paper'
--------------------
-Oh, the computer won. It's ok.
--------------------
-Thanks for playing. Please play again!
+print("-------------------")
+print("Welcome 'Player One' to a Rock-Paper-Scissors game with Nat!")
+print("-------------------")
+
+human_choice = input("Please select either rock, paper, or scissors")
+print("You chose:", human_choice)
+
+exit()
+
+print("You chose: rock")
+print("The computer chose: paper")
+print("-------------------")
+print("Oh, the computer won. It's ok.")
+print("-------------------")
+print("Thanks for playing. Please play again!")
