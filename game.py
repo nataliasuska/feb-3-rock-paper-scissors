72450da1514e407f6912f337c1921b37d15da346
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-02-08 18:12:51 -0500

    Eventual utputs we want
    Well...I wasn't adding any commits because I would save locally from time to time while working on this earlier today. As soon as I finished just now, I saw the rubric required multiple commits...so here we go :)

---
+++ game.py
@@ -0,0 +1,11 @@
+
+-------------------
+Welcome 'Player One' to my Rock-Paper-Scissors game...
+-------------------
+Please choose either 'rock', 'paper', or 'scissors': rock
+You chose: 'rock'
+The computer chose: 'paper'
+-------------------
+Oh, the computer won. It's ok.
+-------------------
+Thanks for playing. Please play again!
