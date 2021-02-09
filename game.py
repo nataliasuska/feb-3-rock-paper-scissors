079e8aa69812bc2600a4c2f3a6e56c895e78e2a7
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-02-08 19:03:54 -0500

    Added in requirements.txt, some syntax updates, and naming code

--- game.py
+++ game.py
@@ -1,47 +1,47 @@
 import random
 import os
+from dotenv import load_dotenv
 
-
-
-human_name = os.getenv("human_name", default="Player One")
+load_dotenv()
+human_name = os.getenv("human_name", default="human")
 
 print("-------------------")
 print(f"Welcome {human_name} to a Rock-Paper-Scissors game with Nat!")
 print("-------------------")
 
 while True:
-human_choice = input("Please select either rock, paper, or scissors:")
-print(f"You chose: {human_choice}")
-human_choice = human_choice.lower()
+    human_choice = input("Please select either rock, paper, or scissors:")
+    print(f"You chose: {human_choice}")
+    human_choice = human_choice.lower()
 
-choices = ['rock', 'paper', 'scissors']
-natbot_choice = random.choice(choices)
-print(f"Nat chose: {natbot_choice}")
+    choices = ['rock', 'paper', 'scissors']
+    natbot_choice = random.choice(choices)
+    print(f"Nat chose: {natbot_choice}")
 
-if human_choice not in choices:
-    print("Please select from given criteria of rock, paper, or scissors and try again.")
-    exit()
-print("-------------------")
+    if human_choice not in choices:
+        print("Please select from given criteria of rock, paper, or scissors and try again.")
+        exit()
+    print("-------------------")
 
-if natbot_choice == "scissors" and human_choice == "rock":
-    print("Congrats! You Won!")
-elif natbot_choice == "rock" and human_choice == "paper":
-    print("Congrats! You Won!")
-elif natbot_choice == "paper" and human_choice == "scissors":
-    print("Congrats! You Won!")
-elif natbot_choice == human_choice:
-    print("It's a tie!")
-else:
-    print("Oh, Nat won. Please don't cry...")
-
-while True:
-    answer = str(input('Do you want to play again? Say yes or no:'))
-    if answer in ('yes','no'):
-        break
-    print("Please write either 'yes' or 'no':)
-    if answer == 'yes':
-        continue
+    if natbot_choice == "scissors" and human_choice == "rock":
+        print("Congrats! You Won!")
+    elif natbot_choice == "rock" and human_choice == "paper":
+        print("Congrats! You Won!")
+    elif natbot_choice == "paper" and human_choice == "scissors":
+        print("Congrats! You Won!")
+    elif natbot_choice == human_choice:
+        print("It's a tie!")
     else:
-        print("-------------------")
-        print("Thanks for playing, I hope you had fun here. Please play again soon!")
-        print("-------------------")
+        print("Oh, Nat won. Please don't cry...")
+
+    while True:
+        answer = str(input('Do you want to play again? Say yes or no:'))
+        if answer in ('yes','no'):
+            break
+        print("Please write either 'yes' or 'no':")
+        if answer == 'yes':
+            continue
+        else:
+            print("-------------------")
+            print("Thanks for playing, I hope you had fun here. Please play again soon!")
+            print("-------------------")
